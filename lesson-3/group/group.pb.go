0e6eaad68b57b555ee55013c365937fa6cec7d35
Mintesnot Afework <MintesnotAfework12@gmail.com>
2025-06-29 17:39:54 +0300

    fixxing import issue

--- lesson-3/group/group.pb.go
+++ lesson-3/group/group.pb.go
@@ -7,12 +7,13 @@
 package group
 
 import (
-	user "/user"
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
+
+	user "github.com/mintesnotAfework/protocol-buffer/user"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
